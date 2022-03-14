6cdc85c8908e20d47a8036729aef85509f8cb2ca
Blaine Dannheisser <blainecd@gmail.com>
2022-03-14 15:21:35 -0400

    Add EditView-ViewModel to handle logic.

--- Project_14_BucketList/Project_14_BucketList/EditView.swift
+++ Project_14_BucketList/Project_14_BucketList/EditView.swift
@@ -11,31 +11,23 @@ import SwiftUI
 
 struct EditView: View {
 
-    enum LoadingState {
-        case loading, loaded, failed
-    }
+    @StateObject private var editViewModel: EditViewModel
 
     @Environment(\.dismiss) var dismiss
-    var location: Location
+
     var onSave: (Location) -> Void
-
-    @State private var name: String
-    @State private var description: String
-
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
 
     var body: some View {
         NavigationView {
             Form {
                 Section("Location Details:") {
-                    TextField("Place Name", text: $name)
-                    TextField("Description", text: $description)
+                    TextField("Place Name", text: $editViewModel.name)
+                    TextField("Description", text: $editViewModel.description)
                 }
                 Section("Nearby...") {
-                    switch loadingState {
+                    switch editViewModel.loadingState {
                     case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
+                        ForEach(editViewModel.pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ")
@@ -52,17 +44,13 @@ struct EditView: View {
             .navigationTitle("Location Details")
             .toolbar {
                 Button("Save") {
-                    var newLocation = location
-                    newLocation.id = UUID()
-                    newLocation.name = name
-                    newLocation.description = description
-
+                    let newLocation = editViewModel.createNewLocation()
                     onSave(newLocation)
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await editViewModel.fetchNearbyPlaces()
             }
         }
     }
@@ -70,35 +58,15 @@ struct EditView: View {
     // MARK: Lifecycle
 
     init(location: Location, onSave: @escaping (Location) -> Void) {
-        self.location = location
         self.onSave = onSave
-
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
-    }
-
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinates.latitude)%7C\(location.coordinates.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            loadingState = .failed
-        }
+        _editViewModel = StateObject(wrappedValue: EditViewModel(location: location, name: "", description: ""))
     }
 }
 
 // MARK: - EditView_Previews
 
-struct EditView_Previews: PreviewProvider {
-    static var previews: some View {
-        EditView(location: Location.example) { newLocation in }
-    }
-}
+//struct EditView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        EditView(location: Location.example) { newLocation in }
+//    }
+//}
